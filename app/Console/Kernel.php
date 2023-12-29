2661b726ce6056a43298de9a932045e5181c6aee
Johan Hoyos <ci83rtda@live.com>
2023-12-28 19:06:26 -0500

    run job on the specific schedule.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\GetDevices;
+use App\Console\Commands\PingDevices;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(GetDevices::class)->everySixHours();
+        $schedule->command(GetDevices::class)->cron('0 0,6,12,18 * * *')->timezone('America/Bogota');
+        $schedule->command(PingDevices::class)->everyFifteenMinutes()->timezone('America/Bogota');
     }
 
     /**
