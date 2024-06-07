be15d0dab7c29a1f8d19dbdf4036ad1682bfd0ac
Johan Hoyos <ci83rtda@live.com>
2024-06-07 03:49:21 -0400

    Add command to test ping and its output.

---
+++ app/Console/Commands/TestPing.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class TestPing extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-ping';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $ip = $this->ask('What is your IP?');
+
+        $process = new Process(["/sbin/ping", "-c 1", escapeshellarg($ip)]);
+        $process->run();
+        $this->info('Output: '.$process->getOutput());
+        $this->info('isSuccessful:  '.$process->isSuccessful());
+
+    }
+}
