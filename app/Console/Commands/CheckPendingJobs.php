f482e763a1f8853a73ba15598465175cf3039359
Johan Hoyos <ci83rtda@live.com>
2024-06-07 05:46:25 -0400

    add check pending jobs command.

---
+++ app/Console/Commands/CheckPendingJobs.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CheckPendingJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:check-pending-jobs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $count = DB::table('jobs')->whereNull('reserved_at')->count();
+        $this->info($count . ' jobs are pending in the queue.');
+
+        if ($count > 0) {
+            $jobs = DB::table('jobs')->whereNull('reserved_at')->get();
+            foreach ($jobs as $job) {
+                $this->line("Job ID: {$job->id}, Job Payload: " . json_decode($job->payload)->displayName);
+            }
+        }
+    }
+}
