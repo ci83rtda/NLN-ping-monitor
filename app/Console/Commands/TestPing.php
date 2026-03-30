4c3119b1cb4f9960855a1742cf822e496fcb41c3
Johan Hoyos <ci83rtda@live.com>
2026-03-30 01:50:19 -0400

    add a way to test ping and update manually.

--- app/Console/Commands/TestPing.php
+++ app/Console/Commands/TestPing.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Device;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
 
@@ -26,13 +28,91 @@ class TestPing extends Command
      */
     public function handle()
     {
-        $ip = $this->ask('What is your IP?');
+        $IpAddress = $this->ask('What is your IP?');
 
-        $process = new Process(["/usr/bin/ping", "-c 1", $ip]);
-        $process->run();
-        $this->info('Output: '.$process->getOutput());
-        $this->info('isSuccessful:  '.$process->isSuccessful());
-        $this->info('IP:  '.$ip);
+        $update = $this->choice('Update the DB?', ['yes', 'no']);
+
+        if($update != 'yes') {
+
+            $process = new Process(["/usr/bin/ping", "-c 1", $IpAddress]);
+            $process->run();
+            $this->info('Output: '.$process->getOutput());
+            $this->info('isSuccessful:  '.$process->isSuccessful());
+            $this->info('IP:  '.$IpAddress);
+
+        }else{
+
+            try {
+                $device = Device::where('ipAddress', $IpAddress)->first();
+                if (is_null($device)) {
+                    return;
+                }
+
+                $process = new Process(["/usr/bin/ping", "-c 1", $IpAddress]);
+                $process->run();
+                $ping = $process->isSuccessful() == 1 ? 1 : 0;
+                //dd($process->getOutput());
+
+//            Log::info("checking {$this->IpAddress}, response {$ping}");
+//            Log::info("output ".json_encode($process->getOutput()));
+
+                if($device->status != $ping){
+                    \Log::info("we update... {$IpAddress}");
+
+                    //$device->update(['query_date' => now()]);
+
+                    $client = new Client([
+                        'base_uri' => config('services.uisp.url'),
+                        'headers' => ['x-auth-token' => config('services.uisp.token')]
+                    ]);
+
+                    $data = [
+                        "deviceId" => $device->deviceId,
+                        "hostname" => $device->hostname,
+                        "modelName" => $device->modelName,
+                        "systemName" => "pi-monitor",
+                        "vendorName" => $device->vendorName,
+                        "ipAddress" => $device->ipAddress,
+                        "macAddress" => $device->macAddress,
+                        "deviceRole" => $device->deviceRole,
+                        "siteId" => $device->siteId,
+                        "pingEnabled" => !($ping == 1),
+                        "ubntDevice" => false,
+                        "ubntData" => [
+                            "firmwareVersion" => "0",
+                            "model" => "blackbox"
+                        ],
+                        "snmpCommunity" => "public",
+                        "note" => "Fiber CPE",
+                        "interfaces" => [
+                            [
+                                "id" => "eth0",
+                                "position" => 0,
+                                "name" => "eth1",
+                                "mac" => $device->macAddress,
+                                "type" => "eth",
+                                "addresses" => $device->cidrIpAddress
+                            ]
+                        ]
+                    ];
+
+                    $response = $client->request('PUT', "devices/blackboxes/{$device->deviceId}/config", ['json' => $data]);
+//                \Log::info(json_encode($response->getBody()->getContents()));
+                    $device->update(['status' => (boolean) $ping, 'query_date' => now()]);
+
+                }
+
+            } catch (\Exception $e) {
+                // Log the exception
+            \Log::error('PingModemIP job failed: ' . $e->getMessage());
+
+                // Optionally rethrow the exception if you want to trigger a retry
+                //throw $e;
+            }
+
+        }
+
+
 
     }
 }
